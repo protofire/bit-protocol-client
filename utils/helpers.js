d2fb35d52f7cc852336a097ff67cd7680e9ee072
Alexandre Ramos <alexandre.rsan@gmail.com>
2025-03-19 10:43:35 -0300

    interest rate added to collateral card

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,31 +1,46 @@
-import BigNumber from "bignumber.js";
+import BigNumber from 'bignumber.js';
 
 export const fromBigNumber = (num, decimals = 1e18) =>
   Number(new BigNumber(num).div(decimals).toFixed());
 
+export const convertInterestRate = (interestRate) => {
+  // Create a BigNumber from the contract value
+  const bnInterestRate = new BigNumber(interestRate);
+
+  // Divide by INTEREST_PRECISION (1e27)
+  const withoutPrecision = bnInterestRate.div(1e27);
+
+  // Multiply by 10000 * SECONDS_IN_YEAR to get the annual rate
+  // SECONDS_IN_YEAR = 31536000 (365 days)
+  const annualRate = withoutPrecision.times(10000).times(31536000);
+
+  // Convert to percentage format with 2 decimal places
+  return (annualRate / 100).toFixed(2);
+};
+
 export const formatNumber = (num) => {
   if (isNaN(num)) return 0;
-  return num.toLocaleString("en-US", {
+  return num.toLocaleString('en-US', {
     maximumFractionDigits: 2,
-    notation: "compact",
-    compactDisplay: "short",
+    notation: 'compact',
+    compactDisplay: 'short',
   });
 };
 
 export const checkProtectedRoute = (path, routes) => {
   if (routes.includes(path)) return true;
-  if (path.startsWith("/Mint/")) return true;
+  if (path.startsWith('/Mint/')) return true;
   return false;
 };
-
 
 export const bnIsBiggerThan = (big = new BigNumber(0), num = 0) => {
   const numBN = new BigNumber(num).multipliedBy(1e18).integerValue().toFixed();
   return big.lt(numBN);
-}
+};
 
 export const inputValueDisplay = (value, balance, isPayable) => {
-  if (value === 0) return "0"
-  if (!isPayable && bnIsBiggerThan(balance, value)) return balance.div(1e18).toString()
-  return value || ""
-}
+  if (value === 0) return '0';
+  if (!isPayable && bnIsBiggerThan(balance, value))
+    return balance.div(1e18).toString();
+  return value || '';
+};
