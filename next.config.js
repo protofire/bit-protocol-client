17423a66588563e73070d8dbb1d630cd5ee4172c
Gera Barboni <gerardobarboni@gmail.com>
2025-08-26 09:13:05 -0300

    redirect to /vault

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/Vault",
+        permanent: false,
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
